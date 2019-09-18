76a80a8a7479247c6e434ec8be07bd516e74f2c4
Juan Soto <jasotojr@gmail.com>
2019-09-18 21:57:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -15,32 +15,32 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
-    first_number = 6 ('./calculator.rb', "first_number")
-    second_number = 4 ('./calculator.rb', "second_number")
+    first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
     sum = get_variable_from_file('./calculator.rb', "sum")
 
     expect(sum).to eq(first_number+second_number)
   end
 
   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
-    first_number = 6 ('./calculator.rb', "first_number")
-    second_number = 4 ('./calculator.rb', "second_number")
+    first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
     difference = get_variable_from_file('./calculator.rb', "difference")
 
     expect(difference).to eq(first_number-second_number)
   end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
-    first_number = 6 ('./calculator.rb', "first_number")
-    second_number = 4 ('./calculator.rb', "second_number")
+    first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
     product = get_variable_from_file('./calculator.rb', "product")
 
     expect(product).to eq(first_number*second_number)
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    first_number = 4 ('./calculator.rb', "first_number")
-    second_number = 5 ('./calculator.rb', "second_number")
+    first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
     quotient = get_variable_from_file('./calculator.rb', "quotient")
 
     expect(quotient).to eq(first_number/second_number)
